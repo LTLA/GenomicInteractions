c87f949b898a6afc1a7089e684b24c2014ddc873
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-05-04 18:42:53 -0700

    Systematic setting of seqinfo for test simulations.

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,17 +1,16 @@
 # library(testthat); library(GenomicInteractions)
 
-chromos <- c("chrA", "chrB")
+chrlens <- c(chrA=1000, chrB=2000)
+chromos <- names(chrlens)
 
 spawn_gi <- function(N1=30, N2=15, Np=50) {
     all.starts <- round(runif(N1, 1, 100))
     all.ends <- all.starts + round(runif(N1, 5, 20))
-    regions1 <- GRanges(sample(chromos, N1, replace=TRUE), IRanges(all.starts, all.ends),
-        seqlengths=c(chrA=1000, chrB=2000))
+    regions1 <- GRanges(sample(chromos, N1, replace=TRUE), IRanges(all.starts, all.ends), seqlengths=chrlens)
 
     all.starts <- round(runif(N2, 1, 100))
     all.ends <- all.starts + round(runif(N2, 5, 20))
-    regions2 <- GRanges(sample(chromos, N2, replace=TRUE), IRanges(all.starts, all.ends),
-        seqlengths=c(chrA=1000, chrB=2000))
+    regions2 <- GRanges(sample(chromos, N2, replace=TRUE), IRanges(all.starts, all.ends), seqlengths=chrlens)
 
     Np <- 20
     all.anchor1 <- sample(N1, Np, replace=TRUE)
@@ -23,7 +22,8 @@ spawn_regions <- function(Ngenes=10) {
     gene.starts <- round(runif(Ngenes, 1, 100))
     gene.ends <- gene.starts + round(runif(Ngenes, 5, 20))
     gene.strand <- sample(c("*", "+", "-"), Ngenes, replace=TRUE)
-    GRanges(sample(chromos, Ngenes, replace=TRUE), IRanges(gene.starts, gene.ends), strand=gene.strand)
+    GRanges(sample(chromos, Ngenes, replace=TRUE), IRanges(gene.starts, gene.ends), 
+        strand=gene.strand, seqlengths=chrlens)
 }
 
 expect_as_if <- function(x, y) {
