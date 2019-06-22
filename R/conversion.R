0c8769d43a6416220310272b4366a1b3e9c7211a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-06-22 10:25:27 -0700

    Removed unnecessary coercion code, extended docs.

--- R/conversion.R
+++ R/conversion.R
@@ -10,11 +10,9 @@
 #'
 #' @details
 #' An automated coercion from a Pairs object to a GenomicInteractions object is not possible,
-#' as the former may not contain two GenomicRanges objects (in which case no standard conversion exists).
+#' as the former may not contain two \linkS4class{GRangesFactor} objects (in which case no standard conversion exists).
 #' Hence the need for the more wordy function to be explicit about the expected inputs.
 #' 
-#' Coercion from a GenomicInteractions to a Pairs object will produce an instance with parallel \linkS4class{GRangesFactor}s.
-#'
 #' @examples
 #' anchor1 <- GRanges(c("chr1", "chr1", "chr1", "chr1"), 
 #'     IRanges(c(10, 20, 30, 20), width=5))
@@ -23,21 +21,15 @@
 #' test <- GenomicInteractions(anchor1, anchor2)
 #'
 #' p <- as(test, "Pairs")
-#' p
-#'
 #' makeGInteractionsFromGRangesPairs(p)
+#'
+#' # Also works with two GRanges.
+#' alt <- Pairs(unfactor(first(p)), unfactor(second(p)))
+#' makeGInteractionsFromGRangesPairs(alt)
 #'
 #' @name conversion
 #' @aliases coerce,GenomicInteractions,Pairs-method
 NULL
-
-#' @importClassesFrom S4Vectors Pairs
-#' @importFrom S4Vectors Pairs first second mcols mcols<-
-setAs("GenomicInteractions", "Pairs", function(from) {
-     out <- Pairs(first(from), second(from), names=names(from))
-     mcols(out) <- mcols(from)
-     out
-})
 
 #' @export
 #' @name conversion
